052a181335a7aa44ac06c1afedb10b4b2baca4a7
KetchuppOfficial <dolgov04@list.ru>
2022-11-17 14:58:47 +0300

    Comparison of floating point values was implemented as a separate header

---
+++ include/floating_point_comparison.hpp
@@ -0,0 +1,26 @@
+#ifndef INCLUDE_FLOATING_POINT_COMPARISON
+#define INCLUDE_FLOATING_POINT_COMPARISON
+
+#include <concepts>
+
+namespace cmp
+{
+
+constexpr double epsilon = 1e-6;
+
+template <std::floating_point T>
+bool are_equal (const T first, const T second,
+                const T zero_diff = epsilon,
+                const T rel_diff  = epsilon)
+{
+    auto diff = std::abs (first - second);
+
+    if (diff < zero_diff)
+        return true;
+    else
+        return (diff < std::max (std::abs (first), std::abs (second)) * rel_diff);
+}
+
+} // namespace cmp
+
+#endif // INCLUDE_FLOATING_POINT_COMPARISON
