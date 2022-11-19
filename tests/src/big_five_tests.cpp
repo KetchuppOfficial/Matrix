28f5d92d8e083be9919ad737d29b09791bc9db68
KetchuppOfficial <dolgov04@list.ru>
2022-11-19 16:20:29 +0300

    std::memcmp replaced with std::equal

--- tests/src/big_five_tests.cpp
+++ tests/src/big_five_tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <vector>
+#include <algorithm>
 
 #include "matrix.hpp"
 
@@ -12,11 +13,11 @@ TEST (Constructors, Basic_Ctor)
     constexpr auto size = n_rows * n_cols;
     Matrix<int> m_1 {n_rows, n_cols};
     const int *arr_1 = new int[size]{};
-    EXPECT_TRUE (std::memcmp (m_1.data (), arr_1, size) == 0);
+    EXPECT_TRUE (std::equal (m_1.data(), m_1.data() + m_1.size(), arr_1));
 
     Matrix<int> m_2 {3, 2, 7};
     const int arr_2[6] = {7, 7, 7, 7, 7, 7};
-    EXPECT_TRUE (std::memcmp (m_2.data (), arr_2, 6) == 0);
+    EXPECT_TRUE (std::equal (m_2.data (), m_2.data() + m_2.size(), arr_2));
 
     #ifdef COMPILATION_FAIL
     struct S {};
@@ -29,15 +30,15 @@ TEST (Constructors, Iterator_Ctor)
     std::vector<int> too_small = {1, 2};
     Matrix<int> m_3 {2, 2, too_small.begin(), too_small.end()};
     const int arr_3[] = {1, 2, 0, 0};
-    EXPECT_TRUE (std::memcmp (m_3.data (), arr_3, sizeof (arr_3)) == 0);
+    EXPECT_TRUE (std::equal (m_3.data(), m_3.data() + m_3.size(), arr_3));
 
     std::vector<int> perfect_fit = {3, 4, 5, 6};
     Matrix<int> m_4 {2, 2, perfect_fit.begin(), perfect_fit.end()};
     const int arr_4[] = {3, 4, 5, 6};
-    EXPECT_TRUE (std::memcmp (m_4.data (), arr_4, sizeof (arr_4)) == 0);
+    EXPECT_TRUE (std::equal (m_4.data(), m_4.data() + m_4.size(), arr_4));
     
     std::vector<int> too_big = {7, 8, 9, 10, 11, 12};
     Matrix<int> m_5 {2, 2, too_big.begin(), too_big.end()};
     const int arr_5[] = {7, 8, 9, 10};
-    EXPECT_TRUE (std::memcmp (m_5.data (), arr_5, sizeof (arr_5)) == 0);
+    EXPECT_TRUE (std::equal (m_5.data (), m_5.data() + m_5.size(), arr_5));
 }
