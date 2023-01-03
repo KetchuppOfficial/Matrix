c02cb8d74ab36154952913ed4fe95fa280eb16a7
KetchuppOfficial <dolgov04@list.ru>
2023-01-03 13:01:33 +0300

    Tests for all methods except for determinant calculation have been implemented

--- tests/src/other_methods.cpp
+++ tests/src/other_methods.cpp
@@ -2,7 +2,7 @@
 
 #include "matrix.hpp"
 
-TEST (Operators, Equality)
+TEST (Other_Methods, Equality)
 {
     yLab::Matrix<int> m_1 = {{1, 2},
                              {3, 4}};
@@ -13,4 +13,56 @@ TEST (Operators, Equality)
     EXPECT_TRUE (m_1 == m_1);
     EXPECT_TRUE (m_1 == m_2);
     EXPECT_TRUE (m_1 != m_3);
+}
+
+TEST (Other_Methods, Are_Congruent)
+{
+    yLab::Matrix<int> m_1 {2, 2};
+
+    yLab::Matrix<int> m_2 {2, 3};
+    EXPECT_FALSE (yLab::Matrix<int>::are_congruent (m_1, m_2));
+
+    yLab::Matrix<int> m_3 {3, 3};
+    EXPECT_FALSE (yLab::Matrix<int>::are_congruent (m_1, m_3));
+
+    yLab::Matrix<int> m_4 {2, 2};
+    EXPECT_TRUE  (yLab::Matrix<int>::are_congruent (m_1, m_4));
+}
+
+TEST (Other_Methods, Is_Square)
+{
+    yLab::Matrix<int> m_1 {2, 2};
+    EXPECT_TRUE (m_1.is_square());
+
+    yLab::Matrix<int> m_2 {5, 2};
+    EXPECT_FALSE (m_2.is_square());
+}
+
+TEST (Other_Methods, Transposition)
+{
+    yLab::Matrix<int> m_1 = {{1, 2, 3},
+                             {4, 5, 6},
+                             {7, 8, 9}};
+    yLab::Matrix<int> m_1_T = {{1, 4, 7},
+                               {2, 5, 8},
+                               {3, 6, 9}};
+    m_1.transpose();
+    EXPECT_TRUE (m_1 == m_1_T);
+
+    yLab::Matrix<int> m_2 = {{1,  2,  3,  4},
+                             {5,  6,  7,  8},
+                             {9, 10, 11, 12}};
+    yLab::Matrix<int> m_2_T = {{1, 5,  9},
+                               {2, 6, 10},
+                               {3, 7, 11},
+                               {4, 8, 12}};
+    m_2.transpose();
+    EXPECT_TRUE (m_2 == m_2_T);
+
+    yLab::Matrix<int> m_3 = {{ 1,  2,  3},
+                             { 4,  5,  6},
+                             { 7,  8,  9},
+                             {10, 11, 12}};
+    auto m_3_copy = m_3;
+    EXPECT_TRUE (m_3 == m_3_copy.transpose().transpose());
 }
