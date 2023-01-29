29d6479fe5ac557f31b05dc95f2a3c970b838dcf
KetchuppOfficial <dolgov04@list.ru>
2023-01-30 00:37:10 +0300

    Generator of tests was implemented the proper way

--- tests/end_to_end/src/test_gen.cpp
+++ tests/end_to_end/src/test_gen.cpp
@@ -7,24 +7,35 @@
 int main (int argc, char *argv[])
 {
     std::string test_dir {argv[1], strlen (argv[1])};
-    int n_matrices = std::atoi (argv[2]);
-    size_t max_size = std::atoi (argv[3]);
-    double max_det = std::atof (argv[4]);
+    std::string ans_dir  {argv[2], strlen (argv[2])};
+    
+    int n_matrices = std::atoi (argv[3]);
+    size_t size    = std::atoi (argv[4]);
+    int max_det    = std::atoi (argv[5]);
 
     std::random_device rd;
     std::mt19937_64 gen (rd());
-    std::uniform_int_distribution<size_t> size (0, max_size - 1);
-    std::uniform_real_distribution<double> det (-max_det, max_det);
+    std::uniform_int_distribution<int> det (-max_det, max_det);
 
     for (auto i = 0; i != n_matrices; ++i)
     {
-        yLab::Random_Matrix<double> matrix {size (gen), det (gen)};
+        auto actual_det = det (gen);
+        yLab::Random_Matrix<int> matrix {size, actual_det};
         
-        std::string file_name {"/test_"};
-        file_name += std::to_string (i);
+        auto test_i = std::to_string (i);
 
-        std::ofstream test_file {test_dir + file_name};
+        std::string test_name {"test_"};
+        test_name += test_i;
+        std::ofstream test_file {test_dir + test_name};
+
+        test_file << size << std::endl;
         test_file << matrix;
+
+        std::string ans_name {"answer_"};
+        ans_name += test_i;
+        std::ofstream ans_file {ans_dir + ans_name};
+
+        ans_file << actual_det << std::endl;
     }
     
     return 0;
