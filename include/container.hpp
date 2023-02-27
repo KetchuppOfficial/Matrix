3fef8903309c2ee254145c0b4311370a15448332
KetchuppOfficial <dolgov04@list.ru>
2023-02-27 17:59:53 +0300

    Code was made more exception safe

--- include/container.hpp
+++ include/container.hpp
@@ -15,24 +15,24 @@ class Array_Buff
 protected:
 
     T *data_;
-    size_t size_;
+    size_t capacity_;
 
-    Array_Buff (const size_t size = 0)
-               : data_{(size == 0) ? nullptr :
-                                     static_cast<T *>(::operator new (sizeof (T) * size))},
-                 size_{size} {}
+    Array_Buff (const size_t count = 0)
+               : data_{(count == 0) ? nullptr :
+                                     static_cast<T *>(::operator new (sizeof (T) * count))},
+                 capacity_{count} {}
 
     Array_Buff (const Array_Buff &rhs) = delete;
     Array_Buff &operator= (const Array_Buff &rhs) = delete;
     
     Array_Buff (Array_Buff &&rhs) noexcept
                : data_{std::exchange (rhs.data_, nullptr)}, 
-                 size_{std::exchange (rhs.size_, 0)} {}
+                 capacity_{std::exchange (rhs.capacity_, 0)} {}
 
     Array_Buff &operator= (Array_Buff &&rhs) noexcept
     {
         std::swap (data_, rhs.data_);
-        std::swap (size_, rhs.size_);
+        std::swap (capacity_, rhs.capacity_);
 
         return *this;
     }
@@ -44,20 +44,28 @@ protected:
 };
 
 template <typename T>
-struct Array : private Array_Buff<T>
+class Array : private Array_Buff<T>
 {   
+public:
+
     using iterator = typename yLab::iterator<T>;
     using const_iterator = typename yLab::const_iterator<T>;
-    
-    Array (const size_t size = 0) : Array_Buff<T>{size}
+
+private:
+
+    size_t size_ = 0;
+
+public:
+
+    Array (const size_t count = 0) : Array_Buff<T>{count}
     {
-        for (auto i = 0; i < size; i++)
+        for (auto i = 0; i < count; i++, size_++)
             std::construct_at (this->data_ + i, T{});
     }
 
-    Array (const Array &rhs) : Array_Buff<T>{rhs.size_}
+    Array (const Array &rhs) : Array_Buff<T>{rhs.capacity_}
     {
-        for (auto i = 0; i < rhs.size_; i++)
+        for (auto i = 0; i < rhs.capacity_; i++, size_++)
             std::construct_at (this->data_ + i, rhs.data_[i]);
     }
 
@@ -71,13 +79,13 @@ struct Array : private Array_Buff<T>
 
     ~Array ()
     {
-        std::destroy (this->data_, this->data_ + this->size_);
+        std::destroy (this->data_, this->data_ + size_);
     }
     
     Array (Array &&rhs) = default;
     Array &operator= (Array &&rhs) = default;
 
-    size_t size () const { return this->size_; }
+    size_t size () const { return this->capacity_; }
 
     const T *data () const & { return this->data_; }
     T *data () & { return this->data_; }
